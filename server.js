22b7d6179bcdb18968cce34b70cdd40710ac11cd
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-01-21 14:37:43 +0200

    server

--- server.js
+++ server.js
@@ -852,6 +852,7 @@ const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 const User = require('./models/userModel');
 const cors = require('cors');
+const itemRoutes = require('./routes/itemRoutes');
 
 
 // Initialize app
@@ -975,6 +976,7 @@ app.post("/verify-otp", (req, res) => {
 
 // Custom API routes
 app.use('/api/auth', authRoutes);
+app.use('/api/items', itemRoutes);
 
 
 
