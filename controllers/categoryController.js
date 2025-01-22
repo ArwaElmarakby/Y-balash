62dab60b992fe897b1d721c5bb1df80a044f9f85
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-01-22 13:07:35 +0200

    add search category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -59,3 +59,22 @@ exports.updateCategory = async (req, res) => {
         res.status(500).json({ message: 'Server error', error });
     }
 };
+
+
+
+exports.searchCategories = async (req, res) => {
+    const { name } = req.query; 
+
+    try {
+        
+        const categories = await Category.find({ name: { $regex: name, $options: 'i' } });
+
+        if (categories.length === 0) {
+            return res.status(404).json({ message: 'No categories found' });
+        }
+
+        res.status(200).json(categories);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+};
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { addCategory, getCategories, deleteCategory, updateCategory } = require('../controllers/categoryController');
+const { addCategory, getCategories, deleteCategory, updateCategory, searchCategories  } = require('../controllers/categoryController');
 
 router.post('/add', addCategory); 
 router.get('/all', getCategories); 
 router.delete('/delete/:id', deleteCategory); 
 router.put('/update/:id', updateCategory); 
+router.get('/search', searchCategories);
 
 module.exports = router;
