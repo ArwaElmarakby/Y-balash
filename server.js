677b2b8e6600c4c7b21e7a95c84628c1675989d6
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-02-18 16:38:47 +0200

    back

--- server.js
+++ server.js
@@ -855,9 +855,6 @@ const User = require('./models/userModel');
 const cors = require('cors');
 const restaurantRoutes = require('./routes/restaurantRoutes');
 const categoryRoutes = require('./routes/categoryRoutes'); 
-const cloudinary = require('cloudinary').v2;
-const multer = require('multer');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 
 // Initialize app
@@ -865,39 +862,6 @@ const app = express();
 
 // Connect to MongoDB
 connectDB();
-
-
-// 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-// 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "uploads", // 
-    allowedFormats: ["jpg", "jpeg", "png"],
-  },
-});
-
-const upload = multer({ storage: storage });
-
-
-
-app.post("/upload", upload.single("image"), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ message: "upload photo please" });
-  }
-
-  res.status(200).json({
-    message: "The image has been uploaded successfully",
-    imageUrl: req.file.path, 
-  });
-});
-
 
 // Middleware
 app.use(bodyParser.json());
