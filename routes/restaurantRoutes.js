709285a88de9f6a3e5862675ef8d7768cf066441
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-02-20 21:18:55 +0200

    image restaurants

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { addRestaurant, getRestaurants, deleteRestaurant, updateRestaurant, searchRestaurants } = require('../controllers/restaurantController');
+const uploadRestaurantImage = require('../config/cloudinary');
 
-router.post('/add', addRestaurant); 
-router.get('/all', getRestaurants); 
+router.post('/add', uploadRestaurantImage.single('image'), addRestaurant); 
+router.get('/all', getRestaurants);
 router.delete('/delete/:id', deleteRestaurant); 
-router.put('/update/:id', updateRestaurant); 
+router.put('/update/:id', uploadRestaurantImage.single('image'), updateRestaurant); 
 router.get('/search', searchRestaurants);
 
 module.exports = router;
