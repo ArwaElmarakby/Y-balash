7926848812e04c4dfcf1687aa551f08f45364cc7
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-02-20 22:09:32 +0200

    image restaurantss

--- models/restaurantModel.js
+++ models/restaurantModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const restaurantSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true }, 
-    imageUrl: { type: String, required: true }
+    imageUrl: { type: String, required: true },
+    description: { type: String }
 });
 
 module.exports = mongoose.model('Restaurant', restaurantSchema);
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,11 +1,46 @@
+// const express = require('express');
+// const router = express.Router();
+// const { addRestaurant, getRestaurants, deleteRestaurant, updateRestaurant, searchRestaurants } = require('../controllers/restaurantController');
+
+// router.post('/add', addRestaurant); 
+// router.get('/all', getRestaurants); 
+// router.delete('/delete/:id', deleteRestaurant); 
+// router.put('/update/:id', updateRestaurant); 
+// router.get('/search', searchRestaurants);
+
+// module.exports = router;
+
+
+
 const express = require('express');
 const router = express.Router();
 const { addRestaurant, getRestaurants, deleteRestaurant, updateRestaurant, searchRestaurants } = require('../controllers/restaurantController');
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
 
-router.post('/add', addRestaurant); 
-router.get('/all', getRestaurants); 
-router.delete('/delete/:id', deleteRestaurant); 
-router.put('/update/:id', updateRestaurant); 
+// تكوين Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// تكوين Multer مع Cloudinary
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'restaurants',  
+        allowedFormats: ['jpg', 'jpeg', 'png'],
+    },
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/add', upload.single('image'), addRestaurant); 
+router.get('/all', getRestaurants);
+router.delete('/delete/:id', deleteRestaurant);
+router.put('/update/:id', updateRestaurant);
 router.get('/search', searchRestaurants);
 
 module.exports = router;
