981fd8309279c729bf5c64dce6e4520d6cdb8ac1
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-03-07 22:12:55 +0200

    locationnnnn

--- controllers/authController.js
+++ controllers/authController.js
@@ -144,49 +144,6 @@ exports.changePassword = async (req, res) => {
 
 
 
-exports.updateLocation = async (req, res) => {
-    const { email, longitude, latitude } = req.body;
-
-    try {
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        user.location = {
-            type: 'Point',
-            coordinates: [longitude, latitude]
-        };
-
-        await user.save();
-        res.status(200).json({ message: 'Location updated successfully', user });
-    } catch (error) {
-        console.error("Error updating location:", error);
-        res.status(500).json({ message: 'Server error' });
-    }
-};
-
-
-
-
-
-exports.getLocation = async (req, res) => {
-    const { email } = req.params;
-
-    try {
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        res.status(200).json({ location: user.location });
-    } catch (error) {
-        console.error("Error fetching location:", error);
-        res.status(500).json({ message: 'Server error' });
-    }
-};
-
-
 
 
 
