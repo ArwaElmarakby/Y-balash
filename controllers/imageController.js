9dadfa54fcce70c7131370de67fbd923e239ddd4
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-03-08 15:24:19 +0200

    iddd

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -92,7 +92,7 @@ exports.getImages = async (req, res) => {
   };
 
   exports.deleteImage = async (req, res) => {
-    const { id } = req.body;
+    const { id } = req.params;
   
     try {
       const image = await Image.findByIdAndDelete(id);
@@ -111,7 +111,8 @@ exports.getImages = async (req, res) => {
         return res.status(500).json({ message: "Image upload failed", error: err });
       }
   
-      const { id, name, quantity, price } = req.body;
+      const { id } = req.params;
+      const { name, quantity, price } = req.body;
       const imageUrl = req.file ? req.file.path : null; // Get Cloudinary image URL
   
       try {
--- server.js
+++ server.js
@@ -1091,4 +1091,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 
-// arwaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// arwaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
