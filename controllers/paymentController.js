35dd44d1013ca525e620d520756d9227db38f6f7
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-04-18 23:28:55 +0200

    paymentt

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -41,6 +41,63 @@
 
 
 
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+// const Cart = require('../models/cartModel'); 
+
+// exports.createPayment = async (req, res) => {
+//     const userId = req.user.id; 
+
+//     try {
+//         const cart = await Cart.findOne({ userId })
+//             .populate('items.itemId')
+//             .populate('offers.offerId');
+//         if (!cart) {
+//             return res.status(404).json({ message: 'Cart not found' });
+//         }
+
+//         // Calculate total items price
+//         let totalItemsPrice = 0;
+//         cart.items.forEach(item => {
+//             totalItemsPrice += item.quantity * parseFloat(item.itemId.price);
+//         });
+
+//         // Calculate total offers price
+//         let totalOffersPrice = 0;
+//         cart.offers.forEach(offer => {
+//             totalOffersPrice += offer.quantity * parseFloat(offer.offerId.price);
+//         });
+
+//         // Additional costs
+//         const shippingCost = 50; 
+//         const importCharges = (totalItemsPrice + totalOffersPrice) * 0.1; 
+
+//         // Total price calculation
+//         const totalPrice = totalItemsPrice + totalOffersPrice + shippingCost + importCharges;
+
+//         // Create payment intent with the total price
+//         const paymentIntent = await stripe.paymentIntents.create({
+//             amount: totalPrice * 100, // Convert to cents
+//             currency: 'usd', 
+//             payment_method_types: ['card'], 
+//             metadata: {
+//                 userId: userId, 
+//             },
+//         });
+
+//         res.status(200).json({ clientSecret: paymentIntent.client_secret });
+//     } catch (error) {
+//         res.status(500).json({ message: 'Payment failed', error: error.message });
+//     }
+// };
+
+
+
+
+
+
+
+
+
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Cart = require('../models/cartModel'); 
 
@@ -69,7 +126,7 @@ exports.createPayment = async (req, res) => {
 
         // Additional costs
         const shippingCost = 50; 
-        const importCharges = (totalItemsPrice + totalOffersPrice) * 0.1; 
+        const importCharges = totalItemsPrice / 4; // Calculate import charges as 1/4 of total items price
 
         // Total price calculation
         const totalPrice = totalItemsPrice + totalOffersPrice + shippingCost + importCharges;
