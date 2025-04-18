d0d9bfe2c85f6979ab6a5774f8f39525f03e53f1
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-04-18 21:03:29 +0200

    paymenttt

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,4 +1,46 @@
 
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+// const Cart = require('../models/cartModel'); 
+
+// exports.createPayment = async (req, res) => {
+//     const userId = req.user.id; 
+
+//     try {
+      
+//         const cart = await Cart.findOne({ userId }).populate('items.itemId');
+//         if (!cart) {
+//             return res.status(404).json({ message: 'Cart not found' });
+//         }
+
+       
+//         let totalAmount = 0;
+//         cart.items.forEach(item => {
+//             totalAmount += item.quantity * parseFloat(item.itemId.price);
+//         });
+
+        
+//         const paymentIntent = await stripe.paymentIntents.create({
+//             amount: totalAmount * 100, 
+//             currency: 'usd', 
+//             payment_method_types: ['card'], 
+//             metadata: {
+//                 userId: userId, 
+//             },
+//         });
+
+//         res.status(200).json({ clientSecret: paymentIntent.client_secret });
+//     } catch (error) {
+//         res.status(500).json({ message: 'Payment failed', error: error.message });
+//     }
+// };
+
+
+
+
+
+
+
+
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Cart = require('../models/cartModel'); 
 
@@ -6,21 +48,35 @@ exports.createPayment = async (req, res) => {
     const userId = req.user.id; 
 
     try {
-      
-        const cart = await Cart.findOne({ userId }).populate('items.itemId');
+        const cart = await Cart.findOne({ userId })
+            .populate('items.itemId')
+            .populate('offers.offerId');
         if (!cart) {
             return res.status(404).json({ message: 'Cart not found' });
         }
 
-       
-        let totalAmount = 0;
+        // Calculate total items price
+        let totalItemsPrice = 0;
         cart.items.forEach(item => {
-            totalAmount += item.quantity * parseFloat(item.itemId.price);
+            totalItemsPrice += item.quantity * parseFloat(item.itemId.price);
         });
 
-        
+        // Calculate total offers price
+        let totalOffersPrice = 0;
+        cart.offers.forEach(offer => {
+            totalOffersPrice += offer.quantity * parseFloat(offer.offerId.price);
+        });
+
+        // Additional costs
+        const shippingCost = 50; 
+        const importCharges = (totalItemsPrice + totalOffersPrice) * 0.1; 
+
+        // Total price calculation
+        const totalPrice = totalItemsPrice + totalOffersPrice + shippingCost + importCharges;
+
+        // Create payment intent with the total price
         const paymentIntent = await stripe.paymentIntents.create({
-            amount: totalAmount * 100, 
+            amount: totalPrice * 100, // Convert to cents
             currency: 'usd', 
             payment_method_types: ['card'], 
             metadata: {
