d66285e8882f777701174702c180bca636ca90a9
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-04-20 20:24:00 +0200

    address

--- models/addressModel.js
+++ models/addressModel.js
@@ -1,13 +1,15 @@
-
 const mongoose = require('mongoose');
 
 const addressSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-    street: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    postalCode: { type: String, required: true },
-    country: { type: String, required: true },
-});
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  fullName: { type: String, required: true },
+  mobileNumber: { type: String, required: true },
+  country: { type: String, required: true },
+  streetName: { type: String, required: true },
+  buildingNameNo: { type: String, required: true },
+  cityArea: { type: String, required: true },
+  governorate: { type: String, required: true },
+  nearestLandmark: { type: String }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Address', addressSchema);
--- routes/deliveryRoutes.js
+++ routes/deliveryRoutes.js
@@ -1,19 +1,31 @@
-// routes/deliveryRoutes.js
+// // routes/deliveryRoutes.js
+// const express = require('express');
+// const router = express.Router();
+// const { addAddress, getAddresses, updateAddress, deleteAddress } = require('../controllers/deliveryController');
+// const { authMiddleware } = require('./authRoutes');
+
+// // Add a new address
+// router.post('/address', authMiddleware, addAddress);
+
+// // Get all addresses for a user
+// router.get('/addresses', authMiddleware, getAddresses);
+
+// // Update an address
+// router.put('/address/:addressId', authMiddleware, updateAddress);
+
+// // Delete an address
+// router.delete('/address/:addressId', authMiddleware, deleteAddress);
+
+// module.exports = router;
+
+
+
+
 const express = require('express');
 const router = express.Router();
-const { addAddress, getAddresses, updateAddress, deleteAddress } = require('../controllers/deliveryController');
+const { addAddress } = require('../controllers/deliveryController');
 const { authMiddleware } = require('./authRoutes');
 
-// Add a new address
-router.post('/address', authMiddleware, addAddress);
-
-// Get all addresses for a user
-router.get('/addresses', authMiddleware, getAddresses);
-
-// Update an address
-router.put('/address/:addressId', authMiddleware, updateAddress);
-
-// Delete an address
-router.delete('/address/:addressId', authMiddleware, deleteAddress);
+router.post('/add-address', authMiddleware, addAddress);
 
 module.exports = router;
