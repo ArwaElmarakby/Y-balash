85f320fe118ac60e4582cccdc299abd4d6fff35b
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-04-21 15:28:22 +0200

    pointss

--- models/userModel.js
+++ models/userModel.js
@@ -38,7 +38,6 @@ const userSchema = new mongoose.Schema({
     birthday: { type: Date },
     profileImage: { type: String },
     cart: { type: mongoose.Schema.Types.ObjectId, ref: 'Cart' },
-    points: { type: Number, default: 0 }
 });
 
 userSchema.pre('save', async function (next) {
