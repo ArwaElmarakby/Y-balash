a00697eec5ec35b49a0a0e128fec55722e7bbf90
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-04-21 16:04:50 +0200

    back

--- routes/pointsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addPoints, getUserPoints } = require('../controllers/pointsController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/add', authMiddleware, addPoints);
-router.get('/:userId', authMiddleware, getUserPoints);
-
-module.exports = router;
