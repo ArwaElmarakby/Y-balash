25f874712276b7bbaa2369c46fc96699fd4b7449
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-04-27 19:45:10 +0300

    status of orders backkk

--- models/orderModel.js
+++ models/orderModel.js
@@ -4,12 +4,8 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
     restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant', required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User ', required: true },
-    items: [{
-        itemId: { type: mongoose.Schema.Types.ObjectId, ref: 'Image', required: true },
-        quantity: { type: Number, required: true },
-    }],
+    items: [{ itemId: { type: mongoose.Schema.Types.ObjectId, ref: 'Image' }, quantity: Number }],
     totalAmount: { type: Number, required: true },
-    status: { type: String, enum: ['pending', 'shipped', 'delivered'], default: 'pending' },
     createdAt: { type: Date, default: Date.now }
 });
 
--- routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/orderRoutes.js
-const express = require('express');
-const router = express.Router();
-const { getRecentOrders } = require('../controllers/orderController');
-
-
-router.get('/recent', getRecentOrders);
-
-module.exports = router;
