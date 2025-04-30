cd8110af3be132901c351b23c249425c55562cc5
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-05-01 00:12:52 +0300

    re

--- models/restaurantModel.js
+++ models/restaurantModel.js
@@ -13,7 +13,8 @@ const restaurantSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   imageUrl: { type: String, required: true },
   description: { type: String, required: true },
-  images: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Image' }]
+  images: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Image' }],
+  balance: { type: Number, default: 0 } 
 });
 
 module.exports = mongoose.model('Restaurant', restaurantSchema);
