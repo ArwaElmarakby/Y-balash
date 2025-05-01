819926cd87dddd7a6673a859f2551311409b9f8b
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-05-01 22:47:34 +0300

    recent

--- models/restaurantModel.js
+++ models/restaurantModel.js
@@ -15,7 +15,25 @@ const restaurantSchema = new mongoose.Schema({
   description: { type: String, required: true },
   images: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Image' }],
   balance: { type: Number, default: 0 },
-  pendingWithdrawal: { type: Number, default: 0 }
+  pendingWithdrawal: { type: Number, default: 0 },
+  payouts: [payoutSchema]
+});
+
+
+
+const payoutSchema = new mongoose.Schema({
+  date: { type: Date, default: Date.now },
+  amount: { type: Number, required: true },
+  paymentMethod: { 
+    type: String, 
+    enum: ['Bank Transfer', 'PayPal', 'Vodafone Cash', 'Other'],
+    required: true 
+  },
+  status: {
+    type: String,
+    enum: ['paid', 'pending'],
+    default: 'pending'
+  }
 });
 
 module.exports = mongoose.model('Restaurant', restaurantSchema);
--- routes/sellerRoutes.js
+++ routes/sellerRoutes.js
@@ -569,4 +569,11 @@ router.get('/current-balance', authMiddleware, sellerMiddleware, async (req, res
   );
   
 
+
+  router.get('/payouts/recent',
+    authMiddleware,
+    sellerMiddleware,
+    sellerController.getRecentPayouts
+  );
+
 module.exports = router;
