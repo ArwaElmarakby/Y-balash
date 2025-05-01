5a2a5960fef06e15b877574b38e4d63eb2beeaec
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-05-01 20:04:43 +0300

    month

--- routes/sellerRoutes.js
+++ routes/sellerRoutes.js
@@ -546,12 +546,4 @@ router.get('/current-balance', authMiddleware, sellerMiddleware, async (req, res
 
   
 
-  router.get('/earnings/this-month',
-    authMiddleware,
-    sellerMiddleware,
-    sellerController.getThisMonthEarnings
-  );
-
-  
-
 module.exports = router;
