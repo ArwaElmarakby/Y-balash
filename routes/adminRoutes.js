37a653577a836af465503cb79e749358a8fa7650
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-05-09 02:22:59 +0300

    gll

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -667,4 +667,29 @@ router.get('/orders/all', authMiddleware, adminMiddleware, async (req, res) => {
 });
 
 
+
+router.post('/reject-seller', authMiddleware, adminMiddleware, async (req, res) => {
+    const { userId } = req.body;
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User  not found' });
+        }
+
+        user.isSeller = false;
+        user.managedRestaurant = undefined; 
+        await user.save();
+        res.status(200).json({ 
+            message: 'Seller rejected successfully',
+            user: {
+                _id: user._id,
+                email: user.email,
+                isSeller: false
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+});
+
 module.exports = router;
