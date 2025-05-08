e9808fe4f2b8132aedf1b2819f027795483902a0
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-05-08 21:59:01 +0300

    gi

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -541,4 +541,7 @@ router.get('/sellers/:id', authMiddleware, adminMiddleware, async (req, res) => 
     }
 });
 
+
+router.get('/sellers/:sellerId/performance', authMiddleware, adminMiddleware, adminController.getSellerPerformance);
+
 module.exports = router;
