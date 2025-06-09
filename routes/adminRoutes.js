fb05e12e9cf9049386e0311296410ad1553a2dbd
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-06-09 14:10:11 +0300

    u

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -837,7 +837,4 @@ router.get('/approved-sellers', authMiddleware, adminMiddleware, getApprovedSell
 router.post('/approve-seller', authMiddleware, adminMiddleware, approveSeller);
 
 
-router.get('/low-stock-items', authMiddleware, adminMiddleware, getLowStockItems);
-
-
 module.exports = router;
