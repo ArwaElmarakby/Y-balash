67c139f7bc49fbcb2c55cbde5f5c1b075c4855a1
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-06-17 01:53:28 +0300

    udfghj

--- routes/pointsRoutes.js
+++ routes/pointsRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { calculatePoints, getUserPoints } = require('../controllers/pointsController');
+const { calculatePoints, getUserPoints, usePoints } = require('../controllers/pointsController');
 const { authMiddleware } = require('./authRoutes');
 
 router.post('/calculate', authMiddleware, calculatePoints);
 router.get('/', authMiddleware, getUserPoints);
-
+router.post('/use-points', authMiddleware, usePoints);
 module.exports = router;
