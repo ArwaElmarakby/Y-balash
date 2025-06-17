33fd31ac3630a0ab4c1a90057d343370d5cae797
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-06-17 19:57:22 +0300

    udf

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,7 +1,7 @@
 // routes/cartRoutes.js
 const express = require('express');
 const router = express.Router();
-const { addToCart, getCart, updateCartItem, removeCartItem, getCartSummary, applyCoupon , applyPointsDiscount } = require('../controllers/cartController');
+const { addToCart, getCart, updateCartItem, removeCartItem, getCartSummary, applyCoupon  } = require('../controllers/cartController');
 const { authMiddleware } = require('./authRoutes');
 
 router.post('/add', authMiddleware, addToCart);
@@ -10,6 +10,5 @@ router.put('/update', authMiddleware, updateCartItem);
 router.delete('/remove/:param1?/:param2?', authMiddleware, removeCartItem);
 router.get('/summary', authMiddleware, getCartSummary);
 router.post('/apply-coupon', authMiddleware, applyCoupon);
-router.post('/apply-points', authMiddleware, applyPointsDiscount);
 
 module.exports = router;
