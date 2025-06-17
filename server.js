6260dd576dfbc01a331e5903da3d5cb641f40b8a
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-06-17 23:12:18 +0300

    usf

--- server.js
+++ server.js
@@ -1169,6 +1169,7 @@ app.use('/api/admin', sellerRoutes);
 app.use('/api', sellerRoutes); 
 app.use('/api/addresses', clientAddressRoutes);
 app.use('/api/client-info', clientInfoRoutes);
+app.use('/api/points', pointsRoutes);
 
 
 
