90af3aae6884ff9334a27a2457992ae7717176ef
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-06-18 17:17:35 +0300

    udf

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,15 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const { createPayment } = require('../controllers/paymentController');
-const { 
-    confirmCashPayment,
-    completeCashPayment
-} = require('../controllers/orderController');
 const { authMiddleware } = require('./authRoutes'); 
-const sellerMiddleware = require('../middleware/sellerMiddleware');
 
 
 router.post('/payment', authMiddleware, createPayment); 
-router.post('/cash/confirm', authMiddleware, confirmCashPayment); 
-router.post('/cash/complete', authMiddleware, sellerMiddleware, completeCashPayment);
+
 module.exports = router;
