69aa288eb43e25c4a4985fec2f0dbff803475c43
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-06-19 13:24:49 +0300

    ud

--- routes/sellerRoutes.js
+++ routes/sellerRoutes.js
@@ -945,5 +945,11 @@ router.get('/orders/stats',
     sellerMiddleware,
     sellerController.getOrdersStats
 );
+
+router.get('/monthly-earnings-comparison',
+    authMiddleware,
+    sellerMiddleware,
+    sellerController.getMonthlyEarningsComparison
+);
   
 module.exports = router;
