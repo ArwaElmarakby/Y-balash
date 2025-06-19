a6fdf920467e2419eef615f5638b6e6c9c3c22a8
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-06-20 01:31:01 +0300

    udfghjkl

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -841,6 +841,4 @@ router.get('/low-stock-items', authMiddleware, adminMiddleware, getLowStockItems
 
 router.get('/recent-activities', authMiddleware, adminMiddleware, getRecentActivities);
 
-router.get('/sellers-earnings', authMiddleware, adminMiddleware, getSellersEarnings);
-
 module.exports = router;
