f22907d3d41a522d05721957966360e6c9dc934a
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-06-21 21:22:57 +0300

    udcygsyugyugdgdbj

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -233,107 +233,10 @@ exports.createPayment = async (req, res) => {
 
 
 
-// exports.cashPayment = async (req, res) => {
-//     const userId = req.user.id;
-
-//     try {
-//         const cart = await Cart.findOne({ userId })
-//             .populate('items.itemId')
-//             .populate('offers.offerId');
-//         if (!cart) {
-//             return res.status(404).json({ message: 'Cart not found' });
-//         }
-
-//         // Retrieve restaurantId from items or offers
-//         let restaurantId = null;
-//         if (cart.items.length > 0) {
-//             restaurantId = cart.items[0].itemId.restaurant || null;
-//         } 
-//         if (!restaurantId && cart.offers.length > 0) {
-//             restaurantId = cart.offers[0].offerId.restaurant || null;
-//         }
-//         if (!restaurantId) {
-//             return res.status(400).json({ message: 'Unable to determine restaurant from cart items/offers' });
-//         }
-
-//         // Calculate total items price
-//         let totalItemsPrice = 0;
-//         cart.items.forEach(item => {
-//             totalItemsPrice += item.quantity * parseFloat(item.itemId.price);
-//         });
-
-//         // Calculate total offers price
-//         let totalOffersPrice = 0;
-//         cart.offers.forEach(offer => {
-//             totalOffersPrice += offer.quantity * parseFloat(offer.offerId.price);
-//         });
-
-//         // Additional costs
-//         const shippingCost = 50; 
-//         const importCharges = (totalItemsPrice + totalOffersPrice) * 0.1; // Calculate import charges as 1/4 of total items price
-
-//         // Total price calculation
-//         const totalPrice = totalItemsPrice + totalOffersPrice + shippingCost + importCharges;
-
-//         // Create an order
-//         // const order = new Order({
-//         //     userId: userId,
-//         //     restaurantId: restaurantId,
-//         //     items: cart.items,
-//         //     totalAmount: totalPrice,
-//         //     status: 'pending', // Set initial status to pending
-//         //     paymentMethod: 'cash' 
-//         // });
-
-
-//         const order = new Order({
-//             userId: userId,
-//             restaurantId: restaurantId,
-//             items: cart.items.map(item => ({
-//                 itemId: item.itemId._id,
-//                 quantity: item.quantity,
-//                 price: item.itemId.price
-//             })),
-//             totalAmount: totalPrice,
-//             status: 'pending',
-//             paymentMethod: 'cash' 
-//         });
-
-//         await order.save();
-
-//         await updateProductQuantities(cart.items);
-
-        
-//           await createNotification(
-//             req.user._id,
-//             restaurantId,
-//             'new_order',
-//             'New Order Received',
-//             `New cash order #${order._id} for ${totalPrice} EGP`,
-//             order._id
-//         );
-
-//         // Clear the cart after payment
-//         await Cart.deleteOne({ userId });
-
-//         res.status(200).json({ message: 'Cash payment initiated successfully', orderId: order._id });
-//     } catch (error) {
-//         res.status(500).json({ message: 'Cash payment failed', error: error.message });
-//     }
-// };
-
-
-
-
 exports.cashPayment = async (req, res) => {
     const userId = req.user.id;
 
     try {
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: 'User  not found' });
-        }
-
         const cart = await Cart.findOne({ userId })
             .populate('items.itemId')
             .populate('offers.offerId');
@@ -367,21 +270,22 @@ exports.cashPayment = async (req, res) => {
 
         // Additional costs
         const shippingCost = 50; 
-        const importCharges = (totalItemsPrice + totalOffersPrice) * 0.1; // Calculate import charges as 10% of total items price
-
-        // Calculate discount from points
-        let discountFromPoints = 0;
-        if (user.points >= 10) {
-            const possibleDiscounts = Math.floor(user.points / 10);
-            discountFromPoints = possibleDiscounts * 3; // 10 points = 3 EGP
-            user.points -= possibleDiscounts * 10; // Deduct used points
-            await user.save(); // Save updated user points
-        }
+        const importCharges = (totalItemsPrice + totalOffersPrice) * 0.1; // Calculate import charges as 1/4 of total items price
 
         // Total price calculation
-        const totalPrice = totalItemsPrice + totalOffersPrice + shippingCost + importCharges - discountFromPoints;
+        const totalPrice = totalItemsPrice + totalOffersPrice + shippingCost + importCharges;
 
         // Create an order
+        // const order = new Order({
+        //     userId: userId,
+        //     restaurantId: restaurantId,
+        //     items: cart.items,
+        //     totalAmount: totalPrice,
+        //     status: 'pending', // Set initial status to pending
+        //     paymentMethod: 'cash' 
+        // });
+
+
         const order = new Order({
             userId: userId,
             restaurantId: restaurantId,
@@ -396,9 +300,11 @@ exports.cashPayment = async (req, res) => {
         });
 
         await order.save();
+
         await updateProductQuantities(cart.items);
 
-        await createNotification(
+        
+          await createNotification(
             req.user._id,
             restaurantId,
             'new_order',
@@ -415,3 +321,7 @@ exports.cashPayment = async (req, res) => {
         res.status(500).json({ message: 'Cash payment failed', error: error.message });
     }
 };
+
+
+
+
