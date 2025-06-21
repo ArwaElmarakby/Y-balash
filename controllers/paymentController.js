86d8599d4e359240141020bd4b7c6770a5b88e56
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-06-21 17:40:06 +0300

    ucxsagcghcasgc

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -270,7 +270,7 @@ exports.cashPayment = async (req, res) => {
 
         // Additional costs
         const shippingCost = 50; 
-        const importCharges = totalItemsPrice / 44; // Calculate import charges as 1/4 of total items price
+        const importCharges = (totalItemsPrice + totalOffersPrice) * 0.1;// Calculate import charges as 1/4 of total items price
 
         // Total price calculation
         const totalPrice = totalItemsPrice + totalOffersPrice + shippingCost + importCharges;
