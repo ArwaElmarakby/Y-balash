779a139649bcc39ff0ad4156d500c66f3ff3383e
Arwa Elmarakby <115815349+ArwaElmarakby@users.noreply.github.com>
2025-06-27 19:56:12 +0300

    gsd

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -140,19 +140,19 @@ exports.getRestaurants = async (req, res) => {
 
 
 
-// exports.getRestaurantById = async (req, res) => {
-//   const { id } = req.params;
+exports.getRestaurantById = async (req, res) => {
+  const { id } = req.params;
 
-//   try {
-//       const restaurant = await Restaurant.findById(id);
-//       if (!restaurant) {
-//           return res.status(404).json({ message: 'Restaurant not found' });
-//       }
-//       res.status(200).json(restaurant);
-//   } catch (error) {
-//       res.status(500).json({ message: 'Server error', error });
-//   }
-// };
+  try {
+      const restaurant = await Restaurant.findById(id);
+      if (!restaurant) {
+          return res.status(404).json({ message: 'Restaurant not found' });
+      }
+      res.status(200).json(restaurant);
+  } catch (error) {
+      res.status(500).json({ message: 'Server error', error });
+  }
+};
 
 
 exports.addImageToRestaurant = async (req, res) => {
